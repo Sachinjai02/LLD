e0c964e896aedbd41d275dfb6cb3544732157e0d
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-07 23:05:49 +0530

    For MVC models, to avoid data duplicacy in DB prefer HAS-A over IS-A. Also makes querying easier to get all Slots from same table with HAS-A

--- parkinglot/src/main/java/models/ElectricSlot.java
+++ parkinglot/src/main/java/models/ElectricSlot.java
@@ -1,7 +1,9 @@
 package models;
 
-public class ElectricSlot extends Slot {
+public class ElectricSlot extends BaseModel {
     private Meter meter;
+
+    private Slot baseSlot;
 
     public Meter getMeter() {
         return meter;
@@ -9,5 +11,13 @@ public class ElectricSlot extends Slot {
 
     public void setMeter(Meter meter) {
         this.meter = meter;
+    }
+
+    public Slot getBaseSlot() {
+        return baseSlot;
+    }
+
+    public void setBaseSlot(Slot baseSlot) {
+        this.baseSlot = baseSlot;
     }
 }
