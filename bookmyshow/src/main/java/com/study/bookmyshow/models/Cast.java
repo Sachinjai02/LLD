f13a353bd13545cce01747a453f9fb9464044cb8
Sachin Jain <sachin.jain.nsit@gmail.com>
2022-12-13 18:10:18 +0530

    Fixing table names which conflicted with postgres reserved names

--- bookmyshow/src/main/java/com/study/bookmyshow/models/Cast.java
+++ bookmyshow/src/main/java/com/study/bookmyshow/models/Cast.java
@@ -1,10 +1,12 @@
 package com.study.bookmyshow.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="casts")
 public class Cast extends BaseModel{
     private String name;
 }
--- bookmyshow/src/main/java/com/study/bookmyshow/models/User.java
+++ bookmyshow/src/main/java/com/study/bookmyshow/models/User.java
@@ -1,10 +1,12 @@
 package com.study.bookmyshow.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="Users")
 public class User extends BaseModel {
     private String name;
 }
